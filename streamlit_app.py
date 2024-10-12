ff59ec6b97bee5958fcfb50eba4f60a24b2f4de6
nivesh36923 <niveshofficia@gmail.com>
2024-10-12 13:59:42 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,12 @@
 import streamlit as st
+import pandas as pd
 
 st.title('👽MACHINE LEARNING')
 
 st.info('hey! This is L')
+
+
+with st.expander('Data'):
+  st.write('***Raw Date***')
+  df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
+  df
