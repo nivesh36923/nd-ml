d0a6dbef35017058607c06c0720eef4948e0b67b
nivesh36923 <niveshofficia@gmail.com>
2025-03-09 17:59:15 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,3 +42,69 @@ ax.legend()
 
 # Display the plot in Streamlit
 st.pyplot(fig)
+
+df['f1'] = df['Stock_1'].shift(1)
+df['f2'] = df['Stock_1'].shift(2)
+df['f3'] = df['Stock_1'].shift(3)
+df['f4'] = df['Stock_1'].shift(4)
+df['f5'] = df['Stock_1'].shift(5)
+df['f6'] = df['Stock_1'].shift(6)
+df['f7'] = df['Stock_1'].shift(7)
+
+with st.expander('Data'):
+  df
+df.dropna(inplace=True)
+with st.expander('Data'):
+  df
+
+
+train_size = int(len(df)*0.8)
+train,test= df.iloc[:train_size],df.iloc[train_size:]
+
+X_train, y_train = train.drop(['Date','Stock_1','Stock_2','Stock_3','Stock_4','Stock_5'], axis=1), train['Stock_1']
+X_test, y_test = test.drop(['Date','Stock_1','Stock_2','Stock_3','Stock_4','Stock_5'], axis=1), test['Stock_1']
+print(X_train,y_train)
+print(X_test,y_test)
+
+
+model = xgb.XGBRegressor(objective='reg:squarederror', n_estimators=100, learning_rate=0.1, max_depth=3)
+model.fit(X_train, y_train)
+
+
+# y_pred = model.predict(X_test)
+
+
+mse = mean_squared_error(y_test, y_pred)
+st.info(f"Mean Squared Error: {mse:.4f}")
+
+
+
+X_combined = pd.concat([X_train, X_test], ignore_index=True)
+predictions = model.predict(X_combined)
+pred_df = pd.DataFrame(predictions)
+y_combined = pd.concat([y_train, y_test], ignore_index=True)
+with st.expander('y_train'):
+  y_train
+with st.expander('pred_df')
+  pred_df
+
+
+# Plot each stock separately with correct labels and colors
+fig2, ax2 = plt.subplots(figsize=(12, 6))
+ax2.plot(df['Date'], pred_df, label='predicted ($)', color='red')
+
+ax2.plot(df['Date'], y_combined, label='actual ($)', color='blue')
+ax2.plot(df['Date'], df['Stock_1'], label='df($)', color='green')
+# Titles and labels
+ax2.set_title('Stock Prices between 2020-01-01 and 2020-01-30')
+ax2.set_xlabel('Date')
+ax2.set_ylabel('Close ($)')
+
+# Rotate x-axis labels for better readability
+ax2.xticks(rotation=45)
+
+# Add legend
+ax2.legend()
+
+# Show the plot
+st.pyplot(fig2)
